f5b5558023f0f4ba14f43acbf003f3caa294b4ef
viraeron <ebarborenko@gmail.com>
2024-10-09 23:33:15 +0300

    lab1

---
+++ lab1/task3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+seredina= len(list_players) // 2
+first_komanda = list_players[:seredina]
+second_komanda = list_players[seredina:]
+print(first_komanda)
+print(second_komanda)
